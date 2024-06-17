3dbed2ae72e10732de5dc1544c62a733cf8448e0
alfredkaziibwe19@gmail.com <alfredkaziibwe19@gmail.com>
2024-06-17 07:41:37 -0400

    set account management for admin in backendApi route

---
+++ routes/backendApi.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('adminlogin', [AdminController::class,'adminlogin'])->name('adminlogin');
+    Route::post('Adminregister', [AdminController::class,'Adminregister'])->name('Adminregister');
+    Route::post('logoutAdmin', [AdminController::class,'logoutAdmin'])->name('logoutAdmin');
+    Route::get('profileAdmin', [AdminController::class,'profileAdmin'])->name('profileAdmin');
+
+
+
+
+});
